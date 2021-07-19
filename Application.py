57228de1855d475ff23fe27908169e5c71767f11
Michał Wyczarski <64838595+RedC4ke@users.noreply.github.com>
2021-07-19 19:57:36 +0200

    Update Application.py

--- Application.py
+++ Application.py
@@ -4,7 +4,7 @@ import resource
 
 
 if __name__ == '__main__':
-    resource.setrlimit(resource.RLIMIT_AS, (40000, 40960))
+    resource.setrlimit(resource.RLIMIT_DATA, (4000000, 4096000))
 
     model_file = open("model.json", "r", encoding="utf-8")
     model = POSifiedText.POSifiedNewlineText.from_json(json.load(model_file))
