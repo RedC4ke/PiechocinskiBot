f7c53a8c5ecdf9104bbd883d4765e00a8b66d94b
Michał Wyczarski <64838595+RedC4ke@users.noreply.github.com>
2021-07-19 19:50:48 +0200

    memory limit

--- Application.py
+++ Application.py
@@ -1,18 +1,22 @@
 import json
 import POSifiedText
+import resource
 
 
 if __name__ == '__main__':
+    resource.setrlimit(resource.RLIMIT_DATA, (4000000, 4096000))
+
     model_file = open("model.json", "r", encoding="utf-8")
     model = POSifiedText.POSifiedNewlineText.from_json(json.load(model_file))
 
     sentences = []
     output_text = ""
     for i in range(1):
-        sentence = model.make_sentence()
+        sentence = model.make_sentence(tries=10, max_overlap_total=70)
         if sentence not in sentences:
             output_text += "\n" + sentence
         sentences.append(sentence)
-    output_text = output_text.replace(" .", ".").replace(" ,", ",")\
-        .replace(" ;", ";").replace(" …", "").replace(" ?", "?").replace(" !", "!").replace(" ...", "...")
-    print(output_text)
+    output_text = output_text.replace(" .", ".").replace(" ,", ",").replace(" ;", ";").replace(" …", "")\
+        .replace(" ?", "?").replace(" !", "!").replace(" ...", "...").replace("  ", ": ")
+    print(output_text + "\n")
+    input('press enter')
