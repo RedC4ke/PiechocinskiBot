54cc722d80ec532d059c43c91cf229a918a389c2
Michał Wyczarski <64838595+RedC4ke@users.noreply.github.com>
2021-07-21 16:10:27 +0200

    Production ready!

--- src/app.py
+++ src/app.py
@@ -1,6 +1,8 @@
 import json
 import POSifiedText
 import requests
+import schedule
+import time
 import logging
 import os
 from dotenv import load_dotenv
@@ -14,34 +16,56 @@ fb_page_id = 105803968458128
 fb_post_url = 'https://graph.facebook.com/{}/feed'.format(fb_page_id)
 
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 logger.addHandler(AzureLogHandler(
     connection_string=f'InstrumentationKey={instrumentation_key}'
 ))
 
 
-if __name__ == '__main__':
-    logger.info("Bot started!")
+def info(text):
+    print(text)
+    logger.info(text)
 
-    model_file = open("model.json", "r", encoding="utf-8")
-    model = POSifiedText.POSifiedNewlineText.from_json(json.load(model_file))
 
-    sentences = []
-    output_text = ""
-    for i in range(1):
-        sentence = model.make_sentence(tries=10, max_overlap_total=70)
-        if sentence not in sentences:
-            output_text += "\n" + sentence
-        sentences.append(sentence)
-    output_text = output_text.replace(" .", ".").replace(" ,", ",").replace(" ;", ";").replace(" …", "")\
-        .replace(" ?", "?").replace(" !", "!").replace(" ...", "...").replace("  ", ": ")
+def sentence(markovify_model):
+    output_text = None
+    while output_text is None:
+        output_text = model.make_sentence(tries=10, max_overlap_total=70)
+    return output_text.replace(" .", ".").replace(" ,", ",").replace(" ;", ";").replace(" …", "") \
+        .replace(" ?", "?").replace(" !", "!").replace(" ...", "...").replace("  ", ": ").strip()
 
-    print(output_text)
-    logger.warning(output_text)
-#
+
+def facebook(fb_sentence):
     fb_payload = {
-        'message': output_text.strip(),
-        'access_token': fb_token
+       'message': fb_sentence,
+       'access_token': fb_token
     }
     fb_request = requests.post(fb_post_url, data=fb_payload)
-    print(fb_request.text)
-    logger.warning(fb_request.text)
+    info(f"Facebook response: {fb_request.text}")
+
+    return
+
+
+def job(markovify_model):
+    info("Job started!")
+
+    text = sentence(markovify_model)
+    info(f"Sentence: {text}")
+
+    facebook(text)
+
+    return
+
+
+if __name__ == '__main__':
+    info("Bot started!")
+
+    model_file = open(f"model.json", "r", encoding="utf-8")
+    model = POSifiedText.POSifiedNewlineText.from_json(json.load(model_file))
+
+    schedule.every(75).to(180).minutes.do(job(model))
+    info("Job scheduled!")
+
+    while True:
+        schedule.run_pending()
+        time.sleep(60)
