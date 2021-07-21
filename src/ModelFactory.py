41213cf9d88db3f6e467833f19a6183b21a21d1e
Michał Wyczarski <64838595+RedC4ke@users.noreply.github.com>
2021-07-21 23:35:22 +0200

    just updated model settings

--- src/ModelFactory.py
+++ src/ModelFactory.py
@@ -6,9 +6,10 @@ import re
 if __name__ == '__main__':
     with open("sources.txt", "r", encoding="utf-8") as f:
         pattern = re.compile('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
-        text = pattern.sub('', f.read()).replace('.\n', '\n').replace('\n', '. ').replace('?.', '?').replace('!.', '!')
+        text = pattern.sub('', f.read()).replace('.\n', '\n').replace('\n', '. ').replace('?.', '?').replace('!.', '!')\
+            .replace(' .', '.').replace(' ,', ',')
         print(text)
 
-    text_model = POSifiedText.POSifiedNewlineText(text, state_size=2, well_formed=True)
+    text_model = POSifiedText.POSifiedNewlineText(text, state_size=3, well_formed=True)
     output_file = open("model.json", "w", encoding="utf-8")
     json.dump(text_model.to_json(), output_file, ensure_ascii=False, indent=4)
